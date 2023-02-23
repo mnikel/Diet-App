5a08b1678bd2eff19b9390293810d7f0d96b7f11
Marcin Nikel <marcin.positive@gmail.com>
2023-02-23 19:06:11 +0100

    some macros css changes

--- client/src/components/macros.js
+++ client/src/components/macros.js
@@ -4,12 +4,12 @@ import "./macros.css"
 const Macros = ({ macros, kcal}) => {
     return (
         <div className="macros">
-            <p>Macros:</p>
+            <h3>Macros:</h3>
             <ul>
-                <li>Kcal: {kcal}</li>
-                <li>Protein: {macros.protein.value} {macros.protein.unit}</li>
-                <li>Carbs: {macros.carbs.value} {macros.carbs.unit}</li>
-                <li>Fat: {macros.fat.value} {macros.fat.unit}</li>
+                <li><h5>Protein: {macros.protein.value} {macros.protein.unit}</h5> </li>
+                <li><h5>Carbs: {macros.carbs.value} {macros.carbs.unit}</h5> </li>
+                <li><h5>Fat: {macros.fat.value} {macros.fat.unit}</h5> </li>
+                <li><h5>Kcal: {kcal}</h5> </li>
             </ul>
         </div>
     );
