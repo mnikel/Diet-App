f1b3b4ebc7a4ba91f65dd9955f83514b726a1b16
Marcin Nikel <marcin.positive@gmail.com>
2023-02-24 13:52:09 +0100

    added simple dictionary to declinate units

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -12,7 +12,41 @@ const Recipe = ({
   ingredients,
   preparation,
 }) => {
-
+  
+  const declinationEndings = {
+    "łyżka": ["łyżka", "łyżki", "łyżek"],
+    "łyżeczka": ["łyżeczka", "łyżeczki", "łyżeczek"],
+    "szczypta": ["szczypta", "szczypty", "szczypt"],
+    "kromka": ["kromka", "kromki", "kromek"],
+    "plaster": ["plaster", "plastry", "plastrów"],
+    "sztuka": ["sztuka", "sztuki", "sztuk"],
+    "opakowanie": ["opakowanie", "opakowania", "opakowań"],
+    "porcja": ["porcja", "porcje", "porcji"],
+    "garść": ["garść", "garście", "garści"],
+    "szklanka": ["szklanka", "szklanki", "szklanek"],
+    "liść": ["liść", "liście", "liści"],
+    "ząbek": ["ząbek", "ząbki", "ząbków"], 
+  };
+  
+  function formatQuantity(quantity, unit) {
+    const endings = declinationEndings[unit];
+    if ((quantity > 1 && quantity < 2 && unit === "porcja") || (quantity > 2 && quantity < 3 && unit === "porcja")||  (quantity > 3 && quantity < 4 && unit === "porcja") ) {
+      unit = endings[2]
+    }
+    else if ((quantity < 1  && unit === "garść") || (quantity > 1 && quantity < 2 && unit === "garść") || (quantity > 2 && quantity < 3 && unit === "garść") ||  (quantity > 3 && quantity < 4 && unit === "garść") ) {
+      unit = endings[2]
+    }
+    else if (quantity < 1 || (quantity > 1 && quantity <= 4)) {
+      unit = endings[1];
+    } else if (quantity === 1) {
+      unit = endings[0];
+    } else {
+      unit = endings[2];
+    }
+    return `${quantity} ${unit}`;
+  } // ogarnac to lepiej kiedys tam na razie dziala w miare ok 
+  
+  
   return ( // TODO Pomyslec gdzie dokladnie zdjecie
   <div id="recipe">
   <div id="recipe_header">
@@ -30,7 +64,7 @@ const Recipe = ({
     <h3>Ingredients:</h3>
     <ul id="ingredients_list">
       {ingredients.map((ingredient, index) => (
-        <li key={index}><h3>{ingredient.name.toUpperCase()}</h3><p>{ingredient.quantity.value} {ingredient.quantity.unit.toUpperCase()}</p></li>
+        <li key={index}><h3>{ingredient.name.toUpperCase()}</h3><p>{formatQuantity(ingredient.quantity.value, ingredient.quantity.unit)} LUB {ingredient.weight.value} {ingredient.weight.unit}</p></li>
       ))}
     </ul>
   </div>
