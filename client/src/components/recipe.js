61669440ca4bf9377548ee93eb1f0155580d246c
Marcin Nikel <marcin.positive@gmail.com>
2023-02-24 10:27:08 +0100

    changed  recipe style

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -14,35 +14,40 @@ const Recipe = ({
 }) => {
 
   return ( // TODO Pomyslec gdzie dokladnie zdjecie
-    <div id="recipe">
-      <div id="recipe_name_macros">
+  <div id="recipe">
+  <div id="recipe_header">
+    <div id="recipe_name_macros">
       <h3>{recipeName.toUpperCase()}</h3>
-      <div id="image_placeholder">
-      <img id="recipeimg" src={imageUrl} alt={recipeName}/> 
-      </div>
-      {<Macros macros={macros} kcal={kcal}/>}
-      </div>
-      <div id="recipe_ingredients">
-        <h3>Ingredients:</h3>
-        <ul id="ingredients_list">
-          {ingredients.map((ingredient, index) => (
-            <li key={index}><h3>{ingredient.name.toUpperCase()}</h3><p>{ingredient.quantity.value} {ingredient.quantity.unit.toUpperCase()}</p></li> // TODO zmienic dodawanie spojrzec dokladnie jak wyglada strutkura chcemy tu i gramy i kuchenne miary
-          ))}
-        </ul>
-      </div>
-      <div id="recipe_preparation">
-        <h3>Preparation:</h3>
-        <ol id="preparation_list">
-          {preparation.map((step, index) => (
-            <li key={index}>{step}</li> // TODO zmienic dodawanie na pewno musi byc zrobiony .split na array preparation !!
-          ))}
-        </ol>
-        <div id="recipe_buttons">
-      <Button label="ADD TO PLANNER" /* onClick={() => handleAddToPlanner()} */ url=""/>
-      <Button label="VIEW PLANNER"/*  onClick={() => handleViewPlanner()} */ url=""/>
-      </div>
+      <div id="recipe_macros">
+        {<Macros macros={macros} kcal={kcal}/>}
       </div>
     </div>
+    <div id="image_placeholder">
+      <img id="recipeimg" src={imageUrl} alt={recipeName}/> 
+    </div>
+  </div>
+  <div id="recipe_ingredients">
+    <h3>Ingredients:</h3>
+    <ul id="ingredients_list">
+      {ingredients.map((ingredient, index) => (
+        <li key={index}><h3>{ingredient.name.toUpperCase()}</h3><p>{ingredient.quantity.value} {ingredient.quantity.unit.toUpperCase()}</p></li>
+      ))}
+    </ul>
+  </div>
+  <div id="recipe_preparation">
+    <h3>Preparation:</h3>
+    <ol id="preparation_list">
+      {preparation.map((step, index) => (
+        <li key={index}>{step}</li>
+      ))}
+    </ol>
+  </div>
+  <div id="recipe_buttons">
+    <Button label="ADD TO PLANNER" /* onClick={() => handleAddToPlanner()} */ url=""/>
+    <Button label="VIEW PLANNER"/*  onClick={() => handleViewPlanner()} */ url=""/>
+  </div>
+</div>
+
   );
 };
 
