bdd37dbe7b7032ecb2b652c1711b946eaca8e648
Marcin Nikel <marcin.positive@gmail.com>
2023-02-24 19:51:16 +0100

    cosmetics

--- client/src/components/macros.js
+++ client/src/components/macros.js
@@ -4,12 +4,12 @@ import "./macros.css"
 const Macros = ({ macros, kcal}) => {
     return (
         <div className="macros">
-            <h3>Macros:</h3>
+            <h3>Makro:</h3>
             <ul>
-                <li><h5>Protein: {macros.protein.value} {macros.protein.unit}</h5> </li>
-                <li><h5>Carbs: {macros.carbs.value} {macros.carbs.unit}</h5> </li>
-                <li><h5>Fat: {macros.fat.value} {macros.fat.unit}</h5> </li>
-                <li><h5>Kcal: {kcal}</h5> </li>
+                <li><h5>Białko: {macros.protein.value} {macros.protein.unit}</h5> </li>
+                <li><h5>Węglowodany: {macros.carbs.value} {macros.carbs.unit}</h5> </li>
+                <li><h5>Tłuszcz: {macros.fat.value} {macros.fat.unit}</h5> </li>
+                <li><h5>Kalorie: {kcal}</h5> </li>
             </ul>
         </div>
     );
--- client/src/components/recipethumbnail.js
+++ client/src/components/recipethumbnail.js
@@ -3,13 +3,19 @@ import "./recipethumbnail.css"
 import Macros from "./macros";
 const RecipeThumbnail = ({ imageUrl, recipeName, macros, kcal, onClick}) => {
     return (
-      <button id="thumbnailbutton" onClick={onClick}>
       <div id="thumbnail">
-        <img id="thumbnailimg" src={imageUrl} alt={recipeName}/>
+      <button id="thumbnailbutton" onClick={onClick}>
+        <div id="thumbnail_img">
+        <img src={imageUrl} alt={recipeName}/>
+        </div>
+        <div id="thumbnail_name">
         <h3>{recipeName}</h3>
+        </div>
+        <div id="thumbnail_macros">
         <Macros macros={macros} kcal={kcal}/>
-      </div>
+        </div>
       </button>
+      </div>
     );
   };
   
